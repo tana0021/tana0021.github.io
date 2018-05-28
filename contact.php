c45b2a3bcb8d56ffce2cf2812718028607bbfe4c
Katina Tanascon <tana0021@algonquinlive.com>
2018-05-28 02:45:40 -0400

    PHP FORM

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$from = 'From: Portfolio'; 
+$to = 'katina.tanascon@gmail.com'; // send to this address
+$subject = 'Portfolio Contact Form'; //subject line in email
+
+$body = "From: $name\n
+Email: $email\n 
+Message:\n $message";
+
+mail($to, $subject, $body); 
+header("Location: http://tana0021.github.io/contact.html"); /* Redirect browser */
+exit();
+?>
